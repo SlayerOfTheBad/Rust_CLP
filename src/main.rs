46ff76da7b364be179cf5d51b3fed3ff52c10dd3
Slayer <jacob.schoemaker2000@gmail.com>
2021-04-11 13:20:40 +0200

    Extract functionality from main()

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,50 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // First argument is the call to the program
-    // Second argument is the needle
-    // Third argument is the haystack (file name)
-    if args.len() < 3 {
-        println!("Please provide both a needle and a haystack");
-        return;
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = run(&config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
     }
+}
 
-    let config = parse_config(&args);
-
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong when reading the file");
-
+fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(&config.filename)?;
 
     println!("Searching for {} in text:\n{}.", config.query, contents);
+
+    Ok(())
 }
+
 
 struct Config {
     query: String,
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        // First argument is the call to the program
+        // Second argument is the needle
+        // Third argument is the haystack (file name)
+        if args.len() < 3 {
+            return Err("Not enough arguments have been provided");
+        }
 
-    Config { query, filename }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
