f52e073a9166f415894417374c179fc8f0be1b50
Slayer <jacob.schoemaker2000@gmail.com>
2021-04-11 18:22:38 +0200

    Write search() using TDD

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,34 @@ impl Config {
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.filename)?;
 
-    println!("Searching for {} in text:\n{}.", config.query, contents);
-
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
